3b10b7e101fa056a4b7c8b5b2abcbabcac1d625b
Aleksander Hansen <aleksanderhan@gmail.com>
2023-04-03 23:19:14 +0200

    wip

--- gpt.py
+++ gpt.py
@@ -1,18 +1,22 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import tiktoken
+from reformer_pytorch import LSHAttention
+
 
 # hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
+batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' #'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
 eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
+n_embd = 256
+n_head = 5
+n_layer = 5
 dropout = 0.2
 # ------------
 
@@ -25,14 +29,26 @@ with open('input.txt', 'r', encoding='utf-8') as f:
 # here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
+
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
+
+
+enc = tiktoken.get_encoding("gpt3")
+vocab_size = enc.n_vocab
+encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
+decode = lambda l: enc.decode(l)
+print(vocab_size)
+n_embd = vocab_size
+
 # Train and test splits
 data = torch.tensor(encode(text), dtype=torch.long)
+
+
 n = int(0.9*len(data)) # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
@@ -89,6 +105,24 @@ class Head(nn.Module):
         out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
 
+
+class LSHMultiHeadAttention(nn.Module):
+    """ multiple heads of LSH self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([LSHAttention(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B, T, C = x.size()
+        x = x.view(B, -1, T, C).transpose(1, 2)  # B x T x n_head x head_size
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+
 class MultiHeadAttention(nn.Module):
     """ multiple heads of self-attention in parallel """
 
@@ -125,7 +159,7 @@ class Block(nn.Module):
         # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
         head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
+        self.sa = LSHMultiHeadAttention(n_head, head_size)
         self.ffwd = FeedFoward(n_embd)
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
