c5136ae44dfe01fa8f6f2081c013f14c902dc58f
Aleksander Hansen <aleksanderhan@gmail.com>
2023-04-07 16:01:36 +0200

    tokenizer

--- gpt.py
+++ gpt.py
@@ -39,7 +39,7 @@ decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integ
 
 
 
-enc = tiktoken.get_encoding("cl100k_base")
+enc = tiktoken.get_encoding("gpt2") #cl100k_base
 vocab_size = enc.n_vocab
 encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
 decode = lambda l: enc.decode(l)
